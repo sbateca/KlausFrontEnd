587829f3b8a78f6cf39b243cbc2d76f99c264b80
Sergio <sbtk87@hotmail.com>
2020-02-18 12:33:25 -0500

    ajuste de plantillas header, footer, menu y contenidos (versión iniciaL)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContenidoComponent } from './contenido/contenido.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    ContenidoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
