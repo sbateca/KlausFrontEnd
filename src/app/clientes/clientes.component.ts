ab5f7182b5893ab4b8cef852578321deea7999f4
Leo <leoonorio_02@hotmail.com>
2020-02-19 17:18:14 -0500

    Tabla cliente con el diseño escogido de la plantilla

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
-import swal from 'sweetalert2';
+
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
@@ -16,34 +16,4 @@ export class ClientesComponent implements OnInit {
       cliente=>this.cliente=cliente//Actializa listado
     );
   }
- // delete (cliente: Cliente):void{
-   /* const swalWithBootstrapButtons = swal.mixin({
-customClass: {
-confirmButton: 'btn btn-success',
-cancelButton: 'btn btn-danger'
-},
-buttonsStyling: false
-})
-swalWithBootstrapButtons.fire({
-title: 'Estas seguro?',
-text: `¿Seguro que desea eliminar al cliente? ${cliente.nombres} ${cliente.apellidos}`,
-icon: 'warning',
-showCancelButton: true,
-confirmButtonText: 'Si, eliminar!',
-cancelButtonText: 'No, cancelar!',
-reverseButtons: true
-}).then((result) => {
-if (result.value) {*/
-/*this.clienteService.delete(cliente.id).subscribe(
-  response =>{
-    this.cliente = this.cliente.filter(cli=> cli !==cliente)//filtrar que no muestre el cliente que acabamos de eliminar
-    /*swalWithBootstrapButtons.fire(
-      'Cliente Eliminado!',
-      `Cliente ${cliente.nombres} eliminado con éxito.`,
-      'success'
-    )
-  })
-}*/
-//})
 }
-//}
