0552ba731c04a9e82bfaadf89c4b7a1e2f3ee5a2
Leo <leoonorio_02@hotmail.com>
2020-02-25 22:56:08 -0500

    Se cambiaron los iconos de editar y eliminar

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -17,10 +18,43 @@ export class ClientesComponent implements OnInit {
     );
   } 
 
+  /*
+  delete (cliente : Cliente) : void{
+    this.clienteService.delete(cliente.id).subscribe( respuesta => {
+      this.cliente = this.cliente.filter( cli => cli !== cliente )
+     })
+  }*/ 
 
-delete (cliente : Cliente) : void{
-
-  this.clienteService.delete(cliente.id).subscribe( respuesta => { //this.cliente = this.cliente.filter( cli => cli !== cliente )
-})
-} 
-}
+  delete (cliente: Cliente):void{
+    const swalWithBootstrapButtons = swal.mixin({
+    customClass: {
+    confirmButton: 'btn btn-success',
+    cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+    })
+    swalWithBootstrapButtons.fire({
+    title: 'Estas seguro?',
+    text: `¿Seguro que desea eliminar al cliente? ${cliente.nombres} ${cliente.apellidos}`,
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Si, eliminar!',
+    cancelButtonText: 'No, cancelar!',
+    reverseButtons: true
+     })
+    .then((result) => {
+    if (result.value) {
+    this.clienteService.delete(cliente.id).subscribe(
+    response =>{
+    this.cliente = this.cliente.filter(cli=> cli !==cliente)//filtrar que no muestre el cliente que acabamos de eliminar
+    swalWithBootstrapButtons.fire(
+      'Cliente Eliminado!',
+      `Cliente ${cliente.nombres} eliminado con éxito.`,
+      'success'
+                                  )
+                }
+                                                    )
+                      }
+    })
+    }
+  }
